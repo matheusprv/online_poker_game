5f916059900557668a263b49ab932b351afebbfc
Pedro Henrique Oliveira <phleaooliveira@gmail.com>
2023-11-25 15:58:51 -0300

    classe player implementada

--- src/deck.py
+++ src/deck.py
@@ -4,29 +4,29 @@ from card import Card
 class Deck:
     def __init__(self) -> None:
         self.cards = []
-        self.clear_deck()
+        self.createDeck()
 
     # Shuffle the current decks's list of cards
     def shuffle(self) -> None:
         random.shuffle(self.cards)
 
     # Given a number of cards, it will remove from the deck N cards and return it 
-    def distributeCards(self, number_of_cards = 1) -> list:
-        return [self.cards.pop(0) for _ in range(number_of_cards)]
+    def distributeCards(self, numberOfCards = 1) -> list:
+        return [self.cards.pop(0) for _ in range(numberOfCards)]
     
     # Given an array of cards, it will put them back on the deck
-    def returnCard(self, cards_to_return) -> None:
-        for card in cards_to_return:
+    def returnCard(self, cardsToReturn) -> None:
+        for card in cardsToReturn:
             self.cards.append(card)
 
     def printDeck(self) -> None:
         print("Number of cards on deck: " + str(len(self.cards)))
         for card in self.cards:
-            card.print_card()
+            card.printCard()
 
     
     # Begins or restart a deck restarting the cards. The cards aren't shuffle 
-    def clearDeck(self) -> None:
+    def createDeck(self) -> None:
         self.cards.clear()
         self.cards = [Card(value, kind) for value in Card.values for kind in Card.kinds]
 
--- src/player.py
+++ src/player.py
@@ -1,3 +1,62 @@
+from card import Card
+
+import calendar
+import time
+
+
 class Player:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name, chips=1000) -> None:
+        self.name = name
+        self.cards = []
+        self.chips = chips
+        self.wins = 0
+        self.defeats = 0
+
+        currentGMT = time.gmtime()
+        timeStamp = calendar.timegm(currentGMT)
+        self.id = str(timeStamp) + name
+
+    def setActive(self, active) -> None:
+        self.active = active
+
+    def getActive(self) -> bool:
+        return self.active
+    
+    # 1: small blind, 2: big blind, 3: normal
+    def setFunction(self, gameFunction) -> None:
+        self.gameFunction = gameFunction
+    
+    def getFunction(self) -> int:
+        return self.gameFunction
+    
+    def incrementWins(self) -> None:
+        self.wins += 1
+
+    def incrementDefeats(self) -> None:
+        self.defeats += 1
+
+    def getWins(self) -> int:
+        return self.wins
+
+    def getDefeats(self) -> int:
+        return self.defeats
+    
+    def setCards(self, cards) -> None:
+        self.cards = cards
+    
+    def getCards(self) -> [Card]:
+        return self.cards
+
+    def getName(self) -> str:
+        return self.name
+
+    def bet(self, value) -> None:
+        self.chips -= value
+
+    def setChips(self, chips) -> None:
+        self.chips = chips
+
+    def resetPlayer(self) -> None:
+        self.wins = 0
+        self.defeats = 0
+        self.cards = []
